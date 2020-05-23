3aee14aa93356edb1db1c442d9dfc1b1d85e4a79
DmirtyMer <duduff9317@gmail.com>
2020-05-23 17:28:41 +0300

    четвертое ДЗ исправленное

--- script4.js
+++ script4.js
@@ -11,36 +11,27 @@ alert(`Сумма цифр этого числа равна ${sum1}`);
 
 //4 Дз 2 задание
 
-let str2 = prompt('Введите символы');
-let sum2 = '';
-let reg = /./ig;
+var string = 'JavaScript'
 
-for (var i = 0; i < str2.length; i++) {
-
-  if (str2.match(reg)) {
-    sum2 += str2[i].repeat(2);
-  }
-}
-
-alert(sum2);
+alert(string.replace(/r/ig, 'rr'))
 
 
 //4 ДЗ 3 задание
 
 function checkPass() {
-	let pass = prompt('Введите пароль');
+	pass = prompt('Введите пароль');
 
 	let regLow = /[a-z]/g;
 	let regUp = /[A-Z]/g;
 	let regUpLow = /[a-zA-Z]/g;
-	let regDigit = /\d{2}/;
+	let regDigit = /\d{3}/;
 	let regSymb =  /[!,$,#,%]/g;
 
 	(pass.length < 9) ? alert('Длина пароля должна быть от 9 символов') :
 	(!pass.match(regLow)) ? alert('Пароль должен содержать английские буквы нижнего регистра') :
 	(!pass.match(regUp)) ? 	alert('Пароль должен содержать английские буквы верхнего регистра') :	
 	(!pass.match(regUpLow)) ? alert('Пароль должен содержать английские буквы верхнего и нижнего регистра') :
-	(!pass.match(regDigit)) ? alert('Пароль должен содержать не менее 2-х цифр'): 
+	(!pass.match(regDigit)) ? alert('Пароль должен содержать более 2-х цифр'): 
 	(!pass.match(regSymb)) ? alert('Пароль должен содержать один из неалфавитных символов (например, !, $, #, %)') :
 	alert('Пароль введен успешно!');
 }
